8b954c2defa5c5a87f98b95e4b240c94d97bb2da
tejsingh0598 <tejsingh0598@gmail.com>
2026-01-25 19:00:06 +0530

    paymentpagetest

--- api/form/signup.js
+++ api/form/signup.js
@@ -243,7 +243,7 @@ const username = `${firstName.toLowerCase().replace(/\s+/g, '')}${Math.floor(Mat
       email: emailLower,
       tempPassword: randomPassword,
       firstName,
-      redirect: '/thank-you '
+      redirect: '/paymentpagetest'
     });
 
 
