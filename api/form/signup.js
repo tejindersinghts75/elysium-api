c442d372358c09f144a5054b27ef816f2a3c14eb
tejsingh0598 <tejsingh0598@gmail.com>
2026-01-25 10:03:33 +0530

    clerk aut logindsdfadsd

--- api/form/signup.js
+++ api/form/signup.js
@@ -242,7 +242,7 @@ const username = `${firstName.toLowerCase().replace(/\s+/g, '')}${Math.floor(Mat
       email: emailLower,
       tempPassword: randomPassword,
       firstName,
-      redirect: '/dashboard'
+      redirect: '/thank-you'
     });
 
 
