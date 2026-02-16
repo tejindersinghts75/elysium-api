00697dad80907eee7a0928a2dfa1d5491ea951fc
tejsingh0598 <tejsingh0598@gmail.com>
2026-02-16 12:45:31 +0530

    d1

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -71,7 +71,7 @@ async function handleUserData(req, res, clerkUserId) {
         unitReservations = data.PaymentFormData.unit;
       }
 
-      if (data.PaymentFormData?.selectapplication === 99) {
+      if (data.PaymentFormData?.selectapplication === "99") {
         earlyAppStatus = { earlyAppStatus: 'Yes', selectApplication: '99', date: 'July 2025', isFounder: earlyAppStatus.isFounder };
       }
       if (data.isFounder === 'Yes') {
